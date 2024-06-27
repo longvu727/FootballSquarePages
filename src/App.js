a2dcb2d219f20b7d7107bd6e5735638ce883a4f6
Long Vu <longvu727@yahoo.com>
2024-06-27 10:35:16 -0700

    FootballSquarePages: starter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Square({value, onSquareClick}) {
+  return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
 
-export default App;
+export default function Board() {
+  const [squares, setSquares] =  useState([]);
+
+  useEffect( ()=> {
+    fetch("http://localhost:3101/GetGame/d3ada7f0-d02b-499e-b906-136fc0044087")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setSquares(data);
+      })
+      .catch((err) => {
+        console.log(err.message)
+      });
+  }, []);
+
+  function handleClick(i){
+    console.log("Clicked " + i)
+  }
+
+  return (
+    <>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
+      </div>
+    </>
+  )
+}
